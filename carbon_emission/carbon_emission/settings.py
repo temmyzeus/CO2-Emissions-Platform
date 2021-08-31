a5eeb6aaa06b6ee501aa6342ddf217857dbf1885
Awoyele Temiloluwa <awoyeletemiloluwa@gmail.com>
2021-08-31 18:57:23 +0100

    Added `tracking_platform` App to Installed_Apps

--- carbon_emission/carbon_emission/settings.py
+++ carbon_emission/carbon_emission/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tracking_platform.apps.TrackingPlatformConfig'
 ]
 
 MIDDLEWARE = [
