51e28e12ba8e348cdf40a8ead1ed20fa32affc9f
Awoyele Temiloluwa <awoyeletemiloluwa@gmail.com>
2021-09-04 16:39:08 +0100

    Use Template Rendering

--- carbon_emission/routes.py
+++ carbon_emission/routes.py
@@ -1,11 +1,12 @@
 from . import app
 from flask import render_template
-from dash_application import create_dash_application
+from dash_application import create_dash_application, create_dash_locations
 
 create_dash_application(app)
+create_dash_locations(app)
 
 @app.route("/", methods=['GET'])
 @app.route("/home", methods=['GET'])
 @app.route("/index", methods=['GET'])
 def index():
-	return '<h2>Homepage</h2>'
+	return render_template('index.html')
